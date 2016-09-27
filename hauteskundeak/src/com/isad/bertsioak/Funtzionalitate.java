45c6ed39b95fa90c56711f6824f86dfb585deeea
Maider <maider.azanza@gmail.com>
2016-09-27 16:58:32 +0200

    Funtzionalitate berria programatzen... lehenengo bertsioa.

---
+++ hauteskundeak/src/com/isad/bertsioak/Funtzionalitate.java
@@ -0,0 +1,9 @@
+package com.isad.bertsioak;
+
+import java.util.ArrayList;
+
+public class Funtzionalitate {
+	public static void main(String args[]){
+		System.out.println("Hemen kode konplexu bat izan beharko genuke :-) ");
+	}
+}
